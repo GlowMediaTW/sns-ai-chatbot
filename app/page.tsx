f512d04faa3b9a9a2367ad96b96cdc38d779b6e0
jerrycool123 <jerrycool71534@gmail.com>
2025-03-09 20:01:55 +0800

    feat: fix middleware, add README.md

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Button from 'antd/es/button';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 export default function LandingPage() {
@@ -14,11 +15,25 @@ export default function LandingPage() {
     <div className="container d-flex flex-column justify-content-center align-items-center vh-100 text-center">
       <h1 className="mb-3">Welcome to SNS AI Chatbot</h1>
       <p className="text-muted mx-3 mb-4">
-        Easily connect your SNS app with AI to launch your AI chatbot.
+        <span>Easily connect your SNS app with AI to launch your AI chatbot.</span>
       </p>
-      <Button type="primary" size="large" onClick={handleCTAClick}>
-        Get Started
-      </Button>
+      {process.env.NEXT_PUBLIC_MAIN_REPO === 'true' ? (
+        <Link href="https://vercel.com/import/project?template=GITHUB_REPO_URL" target="_blank">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="https://vercel.com/button" alt="Deploy on Vercel" />
+        </Link>
+      ) : (
+        <Button type="primary" size="large" onClick={handleCTAClick}>
+          Get Started
+        </Button>
+      )}
+      <Link
+        href="https://github.com/jerrycool123/sns-ai-chatbot"
+        target="_blank"
+        className="mt-4 text-muted fs-7"
+      >
+        Github Repo
+      </Link>
     </div>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { auth as middleware } from '@/auth';
 
 export const config = {
-  matcher: ['/((?!api|login|_next/static|_next/image|favicon.ico)(?!$).*)'],
+  matcher: ['/((?!api|login|env|_next/static|_next/image|favicon.ico)(?!$).*)'],
 };
