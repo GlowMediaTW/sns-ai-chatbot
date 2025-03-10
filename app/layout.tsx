3fa1e04c4fa7199570103de1515f9ea541258d97
jerrycool123 <jerrycool71534@gmail.com>
2025-03-11 00:46:24 +0800

    fix: deployment link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import '@ant-design/v5-patch-for-react-19';
+import { Metadata } from 'next';
 import { SessionProvider } from 'next-auth/react';
 import { PropsWithChildren } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'SNS AI Chatbot',
+  description: 'Easily connect your SNS app with AI to launch your AI chatbot.',
+};
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,10 @@ export default function LandingPage() {
         <span>Easily connect your SNS app with AI to launch your AI chatbot.</span>
       </p>
       {process.env.NEXT_PUBLIC_MAIN_REPO === 'true' ? (
-        <Link href="https://vercel.com/import/project?template=GITHUB_REPO_URL" target="_blank">
+        <Link
+          href="https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FGlowMediaTW%2Fsns-ai-chatbot&env=ADMIN_USERNAME%2CADMIN_PASSWORD%2CAUTH_SECRET&envDescription=Go+to+the+following+link+for+environment+variables+guide%3A&envLink=https%3A%2F%2Fsns-ai-chatbot.vercel.app%2Fenv&project-name=ai-chatbot&repository-name=ai-chatbot&stores=%5B%7B%22integrationSlug%22%3A%22upstash%22%2C%22productSlug%22%3A%22upstash-kv%22%2C%22type%22%3A%22integration%22%7D%5D"
+          target="_blank"
+        >
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img src="https://vercel.com/button" alt="Deploy on Vercel" />
         </Link>
